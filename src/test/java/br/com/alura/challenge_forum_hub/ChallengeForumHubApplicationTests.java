ec24f565cda36b568709e26671cea1d2bd99453a
Diego Rosas <diegorosas19@gmail.com>
2025-08-19 21:26:32 -0300

    Upando o projeto

---
+++ src/test/java/br/com/alura/challenge_forum_hub/ChallengeForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.challenge_forum_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
